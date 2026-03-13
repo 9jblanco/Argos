d34df87887d1000530c8ce6de058d85ea94c8c34
Blanquito <9jblanco@gmail.com>
2026-03-13 21:37:27 +0100

    Update _masterclass.py
    keeps crahsiong in python 1.9- removed the ,

--- src/multiprocess_pipeline/process/post/_masterclass.py
+++ src/multiprocess_pipeline/process/post/_masterclass.py
@@ -4,4 +4,4 @@ from .._masterclass import BaseProcess
 class PostProcess(BaseProcess):
     def __init__(self,
                  *args, **kwargs) -> None:
-        super(PostProcess, self).__init__(*args, **kwargs,)
+        super(PostProcess, self).__init__(*args, **kwargs)
